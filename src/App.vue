6d7d677d8b64cc5fd194c42091406bc96071601b
Antareza Ghifary <oneakun08@gmail.com>
2025-07-01 21:05:13 +0700

    add specialists

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <Hero/>
   <Service/>
   <About/>
+  <Specialist/>
  </div>
  </template>
  <script setup>
@@ -13,6 +14,7 @@ import Header from '../src/components/Header.vue';
 import Hero from '../src/components/Hero.vue';
 import Service from '../src/components/Service.vue';
 import About from '../src/components/About.vue';
+import Specialist from './components/Specialist.vue';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
